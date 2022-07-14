d05c640d5157f301b68ebf3fa3491abcec2e3d93
Igor Lemões Xavier <ilx.cancao@gmail.com>
2022-07-14 17:45:35 -0300

    Nomeando urls

--- painel/urls.py
+++ painel/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'painel'
+
 urlpatterns = [
-    path('', views.home),
-    path('conexoes/<int:id>/', views.conexoes),
+    path('', views.home, name='dashboard'),
+    path('conexoes/<int:id>/', views.conexoes, name='conexoes'),
 ]
