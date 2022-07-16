3290e8e62a5dfd3e4f7a637a2fbbfdc329444975
Igor Lemões Xavier <ilx.cancao@gmail.com>
2022-07-16 08:00:55 -0300

    Testes com HTMX

--- painel/urls.py
+++ painel/urls.py
@@ -1,10 +1,33 @@
+import json
+import time
+
+import requests
+from django.http import HttpResponse, StreamingHttpResponse
 from django.urls import path
 
 from . import views
+
+
+def retorno_teste(request):    
+    teste = request.GET.get('q')    
+    response=requests.get(f'https://servicodados.ibge.gov.br/api/v1/localidades/estados/{teste}/municipios').json()
+    return HttpResponse(response)
+
+
+def consulta(teste):
+    for i in range(10000):
+        yield '\ndata: {}\n\n'.format('message {}'.format(i+int(teste)))
+        time.sleep(1)
+
+def streamFun(request):
+    teste = request.GET.get('valor')
+    return StreamingHttpResponse(consulta(teste), content_type='text/event-stream')
 
 app_name = 'painel'
 
 urlpatterns = [
     path('', views.home, name='dashboard'),
     path('conexoes/<int:id>/', views.conexoes, name='conexoes'),
+    path('clicked/', retorno_teste, name='clicado'),
+    path('stream/', streamFun, name='stream')
 ]
